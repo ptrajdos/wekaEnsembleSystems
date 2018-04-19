7b412f616ecc689d002af15e99444c94f17f9eec
PT <pawel.trajdos@pwr.edu.pl>
2018-04-19 03:40:45 +0200

    1. Options for CustomizableBagging.wq

--- src/main/java/weka/classifiers/meta/CustomizableBagging.java
+++ src/main/java/weka/classifiers/meta/CustomizableBagging.java
@@ -4,12 +4,21 @@
 package weka.classifiers.meta;
 
 import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Vector;
 
 import weka.classifiers.meta.Bagging;
 import weka.classifiers.meta.customizableBagging.OutputCombiner;
 import weka.classifiers.meta.customizableBagging.OutputCombinerGeneralBased;
+import weka.core.DistanceFunction;
+import weka.core.EuclideanDistance;
 import weka.core.Instance;
+import weka.core.NormalizableDistance;
+import weka.core.Option;
+import weka.core.OptionHandler;
 import weka.core.RevisionUtils;
+import weka.core.Utils;
 
 /**
  * @author pawel
@@ -79,6 +88,10 @@ public class CustomizableBagging extends Bagging {
 	public void setOutCombiner(OutputCombiner outCombiner) {
 		this.outCombiner = outCombiner;
 	}
+	
+	public String outCombinerTipText(){
+		return "Object for comnining outputs of the commitee";
+	}
 
 
 
@@ -88,6 +101,75 @@ public class CustomizableBagging extends Bagging {
 	@Override
 	public String getRevision() {
 		return RevisionUtils.extract("$Revision: 1$");
+	}
+
+
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.meta.Bagging#listOptions()
+	 */
+	@Override
+	public Enumeration<Option> listOptions() {
+		Vector<Option> newVector = new Vector<Option>(1);
+		
+		 newVector.addElement(new Option(
+			      "\tThe distance function to use "+
+		          "(default: weka.classifiers.meta.customizableBagging.OutputCombinerGeneralBased).\n",
+			      "C", 0, "-C"));
+		 
+		 newVector.addAll(Collections.list(super.listOptions()));
+		    
+		return newVector.elements();
+	}
+
+
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.meta.Bagging#setOptions(java.lang.String[])
+	 */
+	@Override
+	public void setOptions(String[] options) throws Exception {
+		String combinerString = Utils.getOption('C', options);
+	    if(combinerString.length() != 0) {
+	      String combinerClassSpec[] = Utils.splitOptions(combinerString);
+	      if(combinerClassSpec.length == 0) { 
+	        throw new Exception("Invalid Distance function " +
+	                            "specification string."); 
+	      }
+	      String className = combinerClassSpec[0];
+	      combinerClassSpec[0] = "";
+
+	      this.setOutCombiner(
+	                  (OutputCombiner) Utils.forName( OutputCombiner.class, 
+	                                 className, 
+	                                 combinerClassSpec)
+	                                        );
+	    }
+	    else 
+	      this.setOutCombiner(new OutputCombinerGeneralBased());
+		
+		super.setOptions(options);
+		
+	}
+
+
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.meta.Bagging#getOptions()
+	 */
+	@Override
+	public String[] getOptions() {
+		Vector<String> options = new Vector<String>();
+	    
+
+	    options.add("-C");
+	    String combinerOptions = (this.outCombiner instanceof OptionHandler)? Utils.joinOptions(((OptionHandler)this.outCombiner).getOptions()):"";
+	    options.add(this.outCombiner.getClass().getName()+" "+combinerOptions); 
+	    
+	    Collections.addAll(options, super.getOptions());
+	    
+	    return options.toArray(new String[0]);
+	    
 	}
 
 	
