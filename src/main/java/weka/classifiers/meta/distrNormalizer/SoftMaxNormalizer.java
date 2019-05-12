a4f1d17f80ce8eefe11274431661aaebd97e74c5
PT <pawel.trajdos@pwr.edu.pl>
2019-05-12 20:54:34 +0200

    1. SoftMaxNormalizer -- no Serializable -- bug fixed.

--- src/main/java/weka/classifiers/meta/distrNormalizer/SoftMaxNormalizer.java
+++ src/main/java/weka/classifiers/meta/distrNormalizer/SoftMaxNormalizer.java
@@ -3,16 +3,18 @@
  */
 package weka.classifiers.meta.distrNormalizer;
 
+import java.io.Serializable;
+
 import weka.core.UtilsPT;
 
 /**
  * Soft-max-based array normalization
  * @author pawel trajdos
  * @since 1.1.0
- * @version 1.1.0
+ * @version 1.1.1
  *
  */
-public class SoftMaxNormalizer implements INormalizer {
+public class SoftMaxNormalizer implements INormalizer, Serializable {
 
 
 	@Override
