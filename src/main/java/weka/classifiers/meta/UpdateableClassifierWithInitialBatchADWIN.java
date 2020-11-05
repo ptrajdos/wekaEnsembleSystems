5383ed357964707f4bba47643b4915dbb06c961c
pawel <pawel.trajdos@pwr.edu.pl>
2020-11-05 17:55:23 +0100

    1. Version 1.9.0 2. UpdateableClassifierWithInitialBatchADWIN has been added + Test. 3. UpdateableClassifierWithInitialBatchTest has been changed -- additional tests.

---
+++ src/main/java/weka/classifiers/meta/UpdateableClassifierWithInitialBatchADWIN.java
@@ -0,0 +1,259 @@
+/**
+ * 
+ */
+package weka.classifiers.meta;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import moa.classifiers.core.driftdetection.ADWIN;
+import weka.classifiers.SingleClassifierEnhancer;
+import weka.classifiers.UpdateableBatchProcessor;
+import weka.classifiers.UpdateableClassifier;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Option;
+import weka.core.Utils;
+import weka.core.UtilsPT;
+import weka.tools.GlobalInfoHandler;
+
+/**
+ * The class implements a wrapper for Updateable classifier that needs initial training batch
+ * @author pawel trajdos
+ * @since 1.9.0
+ * @version 1.9.0
+ *
+ */
+public class UpdateableClassifierWithInitialBatchADWIN extends SingleClassifierEnhancer
+		implements UpdateableBatchProcessor, UpdateableClassifier, GlobalInfoHandler{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6361729013079348002L;
+	
+	/**
+	 * The size of the initial bath.
+	 * Used for initial training.
+	 */
+	protected int initialBatchSize=100;
+	
+	/**
+	 * ADWIN estimator used to predict the size of the window.
+	 */
+	protected ADWIN adwin = new ADWIN();
+	
+	/**
+	 * Determines whether the classifier is in the training mode.
+	 */
+	protected boolean training=true;
+	
+	/**
+	 * Batch of instances for training purposes.
+	 */
+	protected Instances batch;
+	
+	protected int numProcessedInstances;
+	
+
+
+	/**
+	 * 
+	 */
+	public UpdateableClassifierWithInitialBatchADWIN() {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.Classifier#buildClassifier(weka.core.Instances)
+	 */
+	@Override
+	public void buildClassifier(Instances data) throws Exception {
+		int dataSize=data.size();
+		/**
+		 * Initialization with empty data is treated as dataset initialization only. 
+		 */
+		if(  dataSize == 0 ) {
+				this.batch = new Instances(data,0);
+				return;
+		}
+		
+		this.m_Classifier.buildClassifier(data);
+		this.training=false;
+		
+		/**
+		 * Save training set as batch
+		 */
+		this.batch = new Instances(data);
+		this.initialiseAdwin();
+		
+		this.numProcessedInstances=0;
+		
+		if(this.m_Classifier instanceof UpdateableBatchProcessor)
+			((UpdateableBatchProcessor) this.m_Classifier).batchFinished();
+		
+		
+	}
+	
+	protected void initialiseAdwin() throws Exception {
+		Instances tmpValSet = new Instances(this.batch);
+		int tmpValSize = tmpValSet.size();
+		for(int i=0;i<tmpValSize;i++) 
+			this.adwinInstanceCheck(tmpValSet.get(i));
+			
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.UpdateableClassifier#updateClassifier(weka.core.Instance)
+	 */
+	@Override
+	public void updateClassifier(Instance instance) throws Exception {
+		if(this.batch == null) {
+			this.batch = new Instances(instance.dataset(),0);
+		}
+		
+		if(this.training) {
+			
+			this.batch.add(instance);
+			this.numProcessedInstances++;
+			if(this.numProcessedInstances == this.initialBatchSize)
+				this.buildClassifier(this.batch);
+			
+		}else {
+			this.adwinInstanceCheck(instance);
+			if(this.m_Classifier instanceof UpdateableClassifier) 	
+				((UpdateableClassifier) this.m_Classifier).updateClassifier(instance);			
+		}
+
+	}
+	protected void adwinInstanceCheck(Instance instance) throws Exception {
+		boolean isPredictionCorrect = Utils.eq(instance.classValue(), this.classifyInstance(instance));
+		if(this.adwin.setInput(isPredictionCorrect? 0:1)) {
+			this.trimValidationSetADWIN();
+		}
+		
+	}
+	
+	protected void trimValidationSetADWIN() throws Exception {
+		int adwinSize = this.adwin.getWidth();
+		int valSetSize =this.batch.size();
+		if(adwinSize< valSetSize) {
+			for(int i=0;i< valSetSize - adwinSize;i++) 
+				this.batch.remove(0);
+			this.m_Classifier.buildClassifier(this.batch);
+		}
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.UpdateableBatchProcessor#batchFinished()
+	 */
+	@Override
+	public void batchFinished() throws Exception {
+		if(this.training) {
+			this.buildClassifier(this.batch);
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.SingleClassifierEnhancer#listOptions()
+	 */
+	@Override
+	public Enumeration<Option> listOptions() {
+		Vector<Option> newVector = new Vector<Option>(1);
+		
+		 newVector.addElement(new Option(
+			      "\tSize of the initial batch"+
+		          "(default:" + 100  + ").\n",
+			      "IBS", 1, "-IBS"));
+		 
+		 
+		 newVector.addAll(Collections.list(super.listOptions()));
+		    
+		return newVector.elements();
+	}
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.SingleClassifierEnhancer#setOptions(java.lang.String[])
+	 */
+	@Override
+	public void setOptions(String[] options) throws Exception {
+		super.setOptions(options);
+		
+		this.setInitialBatchSize(UtilsPT.parseIntegerOption(options, "IBS", 100));
+	
+	}
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.SingleClassifierEnhancer#getOptions()
+	 */
+	@Override
+	public String[] getOptions() {
+		Vector<String> options = new Vector<String>();
+	    
+
+	    options.add("-IBS");
+	    options.add(""+ this.getInitialBatchSize());
+	    
+	    
+	    Collections.addAll(options, super.getOptions());
+	    
+	    return options.toArray(new String[0]);
+	}
+
+
+	
+
+	/**
+	 * @return the initialBatchSize
+	 */
+	public int getInitialBatchSize() {
+		return this.initialBatchSize;
+	}
+
+	/**
+	 * @param initialBatchSize the initialBatchSize to set
+	 */
+	public void setInitialBatchSize(int initialBatchSize) {
+		this.initialBatchSize = initialBatchSize;
+	}
+
+	public String initialBatchSizeTipText() {
+		return "Size of the initial batch.";
+	}
+
+	
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.AbstractClassifier#distributionForInstance(weka.core.Instance)
+	 */
+	@Override
+	public double[] distributionForInstance(Instance instance) throws Exception {
+		if(this.training) {
+			/**
+			 * Default response if the classifier is not trained yet. 
+			 */
+			int numClasses = instance.numClasses();
+			double[] distribution = new double[numClasses];
+			double prediction = 1.0/numClasses;
+			Arrays.fill(distribution, prediction);
+			return distribution;
+		}
+		return this.m_Classifier.distributionForInstance(instance);
+	}
+
+	@Override
+	public String globalInfo() {
+		return "Wrapper for an updateable classifier that needs initial training";
+	}
+	
+	
+	
+	
+	
+
+}
--- src/test/java/weka/classifiers/meta/UpdateableClassifierWithInitialBatchADWINTest.java
+++ src/test/java/weka/classifiers/meta/UpdateableClassifierWithInitialBatchADWINTest.java
@@ -5,11 +5,13 @@ package weka.classifiers.meta;
 
 import java.util.Random;
 
+import moa.classifiers.core.driftdetection.ADWIN;
 import weka.classifiers.AbstractClassifierTest;
 import weka.classifiers.Classifier;
 import weka.classifiers.UpdateableBatchProcessor;
 import weka.classifiers.bayes.NaiveBayesMultinomialText;
 import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.classifiers.trees.J48;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.FastVector;
@@ -17,6 +19,7 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.Utils;
 import weka.tools.data.RandomDataGenerator;
+import weka.tools.data.WellSeparatedSquares;
 import weka.tools.tests.DistributionChecker;
 import weka.tools.tests.WekaGOEChecker;
 
@@ -26,15 +29,15 @@ import weka.tools.tests.WekaGOEChecker;
  * @version 1.3.0
  *
  */
-public class UpdateableClassifierWithInitialBatchTest extends AbstractClassifierTest {
+public class UpdateableClassifierWithInitialBatchADWINTest extends AbstractClassifierTest {
 
-	public UpdateableClassifierWithInitialBatchTest(String name) {
+	public UpdateableClassifierWithInitialBatchADWINTest(String name) {
 		super(name);
 	}
 
 	@Override
 	public Classifier getClassifier() {
-		return new UpdateableClassifierWithInitialBatch();
+		return new UpdateableClassifierWithInitialBatchADWIN();
 	}
 	
 	public void testEmptySet() {
@@ -54,7 +57,7 @@ public class UpdateableClassifierWithInitialBatchTest extends AbstractClassifier
 	     data.setClassIndex(data.numAttributes()-1);
 	     
 	     
-	     UpdateableClassifierWithInitialBatch testClassifier = new UpdateableClassifierWithInitialBatch();
+	     UpdateableClassifierWithInitialBatchADWIN testClassifier = new UpdateableClassifierWithInitialBatchADWIN();
 	     
 	     
 	     double[] vals = new double[data.numAttributes()];
@@ -114,7 +117,7 @@ public class UpdateableClassifierWithInitialBatchTest extends AbstractClassifier
 	public void testUpdateableTraining() {
 		this.updateableTest(getClassifier());
 		
-		UpdateableClassifierWithInitialBatch classifier = (UpdateableClassifierWithInitialBatch) this.getClassifier();
+		UpdateableClassifierWithInitialBatchADWIN classifier = (UpdateableClassifierWithInitialBatchADWIN) this.getClassifier();
 		classifier.setClassifier(new NaiveBayesUpdateable());
 		this.updateableTest(classifier);
 		
@@ -124,9 +127,8 @@ public class UpdateableClassifierWithInitialBatchTest extends AbstractClassifier
 		RandomDataGenerator gen = new RandomDataGenerator();
 		Instances data = gen.generateData();
 		Instance testInstance = data.get(0);
-		UpdateableClassifierWithInitialBatch classifier = (UpdateableClassifierWithInitialBatch) classifieru;
+		UpdateableClassifierWithInitialBatchADWIN classifier = (UpdateableClassifierWithInitialBatchADWIN) classifieru;
 		classifier.setInitialBatchSize(10);
-		classifier.setRegularBatchSize(10);
 		
 		try {
 		for (Instance instance : data) {
@@ -144,7 +146,7 @@ public class UpdateableClassifierWithInitialBatchTest extends AbstractClassifier
 		RandomDataGenerator gen = new RandomDataGenerator();
 		Instances data = gen.generateData();
 		Instance testInstance = data.get(0);
-		UpdateableClassifierWithInitialBatch classifier = (UpdateableClassifierWithInitialBatch) this.getClassifier();
+		UpdateableClassifierWithInitialBatchADWIN classifier = (UpdateableClassifierWithInitialBatchADWIN) this.getClassifier();
 		try {
 		for(int i=0;i<5;i++) {
 			classifier.updateClassifier(data.get(i));
@@ -157,12 +159,78 @@ public class UpdateableClassifierWithInitialBatchTest extends AbstractClassifier
 		}
 	}
 	
+	public void  testADWINSubObject() {
+		UpdateableClassifierWithInitialBatchADWIN adwClass = (UpdateableClassifierWithInitialBatchADWIN) this.getClassifier();
+		ADWIN adw = adwClass.adwin;
+		int numElements=100;
+		boolean isChanged=false;
+		for(int i=0;i<numElements;i++) {
+			isChanged = adw.setInput(0);
+			if(isChanged)
+				fail("No distribution change should have appeared here!");
+		}
+		
+		boolean anyChangeReported=false;
+		for(int i=0;i<numElements;i++) {
+			isChanged = adw.setInput(1);
+			if(isChanged)
+				anyChangeReported=true;
+		}
+		
+		assertTrue("A change should be reported here", anyChangeReported);
+	}
+	
+	public void testAdwinClassifier() {
+		WellSeparatedSquares gen = new WellSeparatedSquares();
+		gen.setNumClasses(2);
+		gen.setNumObjects(100);
+		int numInstances = gen.getNumObjects();
+		Instances data = gen.generateData();
+		gen.setNumObjects(300);
+		Instances valData = gen.generateData();
+		Instances classRevData = this.changeClassAssignment(valData);
+		UpdateableClassifierWithInitialBatchADWIN adwClassifier = (UpdateableClassifierWithInitialBatchADWIN) this.getClassifier();
+		adwClassifier.setClassifier(new J48());
+		int preInstNum, postInstNum;
+		boolean anyChangeRecorded=false;
+		try {
+			adwClassifier.buildClassifier(data);
+			for(int i=0;i<numInstances;i++) {
+				preInstNum = adwClassifier.adwin.getWidth();
+				adwClassifier.updateClassifier(classRevData.get(i));
+				postInstNum = adwClassifier.adwin.getWidth();
+				if(preInstNum > postInstNum)
+					anyChangeRecorded=true;
+			}
+				
+			assertTrue("Change should have been recorded", anyChangeRecorded);
+			
+		} catch (Exception e) {
+			fail("An exception has been caught: " + e.getMessage());
+		}
+	}
+	
+	protected Instances changeClassAssignment(Instances instances) {
+		Instances newInsts = new Instances(instances);
+		int insNum = newInsts.numInstances();
+		Instance tmpInstance;
+		double classVal;
+		for(int i=0;i<insNum;i++) {
+			tmpInstance =newInsts.get(i);
+			classVal = tmpInstance.classValue();
+			classVal = classVal==0? 1:0;
+			tmpInstance.setClassValue(classVal);
+		}
+		
+		return newInsts;
+	}
+	
 	public void testForceBatchFinish2() {
 		RandomDataGenerator gen = new RandomDataGenerator();
 		gen.setNumObjects(200);
 		Instances data = gen.generateData();
 		Instance testInstance = data.get(0);
-		UpdateableClassifierWithInitialBatch classifier = (UpdateableClassifierWithInitialBatch) this.getClassifier();
+		UpdateableClassifierWithInitialBatchADWIN classifier = (UpdateableClassifierWithInitialBatchADWIN) this.getClassifier();
 		try {
 			classifier.buildClassifier(data);
 			for(int i=0;i<5;i++) {
@@ -185,7 +253,7 @@ public class UpdateableClassifierWithInitialBatchTest extends AbstractClassifier
 		Instances data = gen.generateData();
 		Instance testInstance = data.get(0);
 		
-		UpdateableClassifierWithInitialBatch upda = (UpdateableClassifierWithInitialBatch) this.getClassifier();
+		UpdateableClassifierWithInitialBatchADWIN upda = (UpdateableClassifierWithInitialBatchADWIN) this.getClassifier();
 		UpdateableBatchProcessor updBatch = new NaiveBayesMultinomialText();
 		
 		upda.setClassifier((Classifier) updBatch);
@@ -198,6 +266,7 @@ public class UpdateableClassifierWithInitialBatchTest extends AbstractClassifier
 			fail("An exception has been caught: "  + e.getLocalizedMessage());
 		}
 	}
+	
 	
 
 }
